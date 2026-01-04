f44ddc05d0da841a26fa83a2b5c3e0a124ac0ae3
sanjay <kochrekarsanjay87@gmail.com>
2026-01-04 11:58:35 +0530

    initial project setup

---
+++ crew_chatbot/crew_chatbot/crew_chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  crew_chatbotApp.swift
+//  crew_chatbot
+//
+//  Created by Sanju on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct crew_chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
